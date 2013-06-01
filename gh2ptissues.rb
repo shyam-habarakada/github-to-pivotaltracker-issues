feec00de7b2de144fcb38b54a95925d74b79b798
Shyam Habarakada <shyam.habarakada@gmail.com>
2013-05-31 20:41:55 -0700

    Work in progress

--- gh2ptissues.rb
+++ gh2ptissues.rb
@@ -19,28 +19,18 @@
 # TODO: Exceoption handling. Pivotal create operations just silently fail right now.
 #       You have to just observe your Pivotal project immeidately afterwards.
 
-GITHUB_USER = 'telvue'
-GITHUB_REPO = 'Connect'
-GITHUB_LOGIN = 'rbrooks'
-GITHUB_PASSWORD = '************'
-PIVOTAL_TOKEN = 'ff90000000f123c71e12cca9ead123aa'
-PIVOTAL_PROJECT_ID = '1234567'
+GITHUB_ORG = '***'
+GITHUB_USER = 'shyam-habarakada'
+GITHUB_REPO = '***'
+GITHUB_LOGIN = 'shyam-habarakada'
+GITHUB_PASSWORD = '*******'
+PIVOTAL_TOKEN = '****************'
+PIVOTAL_PROJECT_ID = '******'
 
 require 'rubygems'
 require 'github_api'
 require 'pivotal-tracker'
 
-github = Github.new user: GITHUB_USER, repo: GITHUB_REPO, login: GITHUB_LOGIN, password: GITHUB_PASSWORD
-issues = github.issues.list_repo(GITHUB_USER, GITHUB_REPO,
-  milestone: 8, # Verison 1.1 is 8. Icebox is 6.
-  state: 'open',
-  per_page: 100, # 100 is max.
-  labels: 'Bug', # 'Story', 'Request', 'Bug', 'High', 'Critical'
-  sort: 'created', # 'created', 'updated', 'comments'
-  direction: 'asc' # 'asc', 'desc'. 'Ascending' because we want newest ones prioritized first in Pivotal.
-  # assignee: '*',
-  # mentioned: 'octocat',
-)
 
 # Get a single issue:
 #issue = github.issues.find(GITHUB_USER, GITHUB_REPO, 301)
@@ -49,7 +39,7 @@ issues = github.issues.list_repo(GITHUB_USER, GITHUB_REPO,
 # IOW, some stuff below breaks.
 
 PivotalTracker::Client.token = PIVOTAL_TOKEN
-proj = PivotalTracker::Project.find(PIVOTAL_PROJECT_ID)
+# proj = PivotalTracker::Project.find(PIVOTAL_PROJECT_ID)
 
 # 100 per page is max, so we can iterate all pages like this:
 # issues.each_page do |page|
@@ -61,48 +51,60 @@ proj = PivotalTracker::Project.find(PIVOTAL_PROJECT_ID)
 # any filering above in the list_repo() method options.
 # So we have to break thing up into sub-100 chunks.
 
+github = Github.new org: GITHUB_ORG, repo: GITHUB_REPO, login: GITHUB_LOGIN, password: GITHUB_PASSWORD
+issues = github.issues.list(:filter => 'all', :state => 'open', :sort => 'created')
+
 i = 0
-issues.each_with_index do |issue|
-  puts '*************************************************'
-  puts '*** MIGRATING GITHUB ISSUE to PIVOTAL TRACKER ***'
-  puts 'Owner:       ' + issue.user.login
-  puts 'Assigned to: ' + issue.assignee.login if issue.assignee
-  puts 'Date:        ' + issue.created_at
-  puts 'Story:       ' + issue.title
-  puts '*************************************************'
-  puts issue.body
 
-  # # Create story in Pivotal Tracker
-  story = proj.stories.create(
-    name: issue.title,
-    description: issue.body,
-    created_at: issue.created_at,
-    # labels: 'Request',
-    # owned_by: issue.assignee.login,
-    # requested_by: issue.user.login,
-    story_type: 'bug', # 'bug', 'feature', 'chore', 'release'. Omitting makes it a feature.
-    current_state: 'unstarted' # 'unstarted', 'started', 'accepted', 'delivered', 'finished', 'unscheduled'.
-    # Omitting puts it in the Icebox.
-    # 'unstarted' puts it in 'Current' if Commit Mode is on; 'Backlog' if Auto Mode is on.
-  )
+issues.each_page do |page|
+  page.each do |issue|
+    # labels = issue.labels.select { |k,v| k == "name" }
+    puts "issue: #{issue.title} (#{issue.number})"
 
-  comments = github.issues.comments.all 'telvue', 'Connect', issue.number
-  comments.each do |comment|
-    puts '--------------'
-    puts 'ADDING COMMENT'
-    puts 'From: ' + comment.user.login
-    puts 'Date: ' + comment.created_at
-    puts ''
-    puts comment.body
+    # puts JSON.pretty_generate(issue)
 
-    story.notes.create(
-      text: comment.body.gsub(/\r\n\r\n/, "\n\n"),
-      author: comment.user.login,
-      noted_at: comment.created_at
-    )
+    comments = github.issues.comments.list GITHUB_ORG, GITHUB_REPO, :issue_id => issue.number
+    comments.each_page do |comments_page|
+      comments_page.each do |comment|
+        puts "  comment: #{comment.body}"
+      end
+    end
+
+    # puts issue.body
+
+    # # Create story in Pivotal Tracker
+    # story = proj.stories.create(
+    #   name: issue.title,
+    #   description: issue.body,
+    #   created_at: issue.created_at,
+    #   # labels: 'Request',
+    #   # owned_by: issue.assignee.login,
+    #   # requested_by: issue.user.login,
+    #   story_type: 'bug', # 'bug', 'feature', 'chore', 'release'. Omitting makes it a feature.
+    #   current_state: 'unstarted' # 'unstarted', 'started', 'accepted', 'delivered', 'finished', 'unscheduled'.
+    #   # Omitting puts it in the Icebox.
+    #   # 'unstarted' puts it in 'Current' if Commit Mode is on; 'Backlog' if Auto Mode is on.
+    # )
+
+    # comments = github.issues.comments.all 'telvue', 'Connect', issue.number
+    # comments.each do |comment|
+    #   puts '--------------'
+    #   puts 'ADDING COMMENT'
+    #   puts 'From: ' + comment.user.login
+    #   puts 'Date: ' + comment.created_at
+    #   puts ''
+    #   puts comment.body
+
+      # story.notes.create(
+      #   text: comment.body.gsub(/\r\n\r\n/, "\n\n"),
+      #   author: comment.user.login,
+      #   noted_at: comment.created_at
+      # )
+    # end
+    i = i + 1
   end
-  i = i + 1
 end
+
 puts '=============='
 puts 'TOTAL MIGRATED: ' + i.to_s
 puts '=============='
