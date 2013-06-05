27aaee8d157c76f78e479729bf2c7367ec695819
Shyam Habarakada <shyam.habarakada@gmail.com>
2013-06-05 10:50:27 -0700

    Switch to the new octokit gem
    Move to the official github API library for ruby.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
-gem 'json', '~> 1.7.7'
-gem 'github_api'
-gem 'pivotal-tracker'
+source 'https://rubygems.org'
 
+gem 'json', '~> 1.7.7'
+gem "octokit", "~> 1.24.0"
+gem 'pivotal-tracker'
--- gh2ptissues.rb
+++ gh2ptissues.rb
@@ -1,76 +1,44 @@
 #!/usr/bin/env ruby
 
-# Author:       Russ Brooks, TelVue Corporation (www.telvue.com)
 # Description:  Migrates GitHub Issues to Pivotal Tracker.
-# Dependencies: Ruby 1.9.2+
-#               GitHub API gem: https://github.com/peter-murach/github
-#               Pivtal Tracker gem: https://github.com/jsmestad/pivotal-tracker
-#   1. Change the constants below accordingly for your project.
-#   2. Change the options in list_repo() method for your GitHub project.
-#   3. Change the options in stories.create() method accordingly.
-#   4. Change the options in notes.create() method accordingly.
-#   5. gem install github_api
-#   6. gem install pivotal-tracker
-#   7. Save this code to a .rb file (gh_to_pt.rb), and chmod it executable [775].
-#   8. Run this file: ./gh_to_pt.rb
-#
-# Dry-run it first by commenting the create() methods below.
-#
-# TODO: Exceoption handling. Pivotal create operations just silently fail right now.
-#       You have to just observe your Pivotal project immeidately afterwards.
 
-GITHUB_ORG = '***'
+GITHUB_ORG = 'APX'
 GITHUB_USER = 'shyam-habarakada'
-GITHUB_REPO = '***'
+GITHUB_REPO = 'APX/benchpress-server'
 GITHUB_LOGIN = 'shyam-habarakada'
-GITHUB_PASSWORD = '*******'
-PIVOTAL_TOKEN = '****************'
-PIVOTAL_PROJECT_ID = '******'
+PIVOTAL_PROJECT_ID = '836893'
+
+GITHUB_PASSWORD = ENV['GITHUB_PASSWORD']
+PIVOTAL_TOKEN = ENV['PIVOTAL_TOKEN']
 
 require 'rubygems'
-require 'github_api'
+require 'octokit'
 require 'pivotal-tracker'
 
-
-# Get a single issue:
-#issue = github.issues.find(GITHUB_USER, GITHUB_REPO, 301)
-# Careful when getting a single issue. The signature of the objects
-# returned is different than when iterating issues.list_repo().
-# IOW, some stuff below breaks.
+puts ENV['GITHUB_PASSWORD']
+puts ENV['PIVOTAL_TOKEN']
 
 PivotalTracker::Client.token = PIVOTAL_TOKEN
-# proj = PivotalTracker::Project.find(PIVOTAL_PROJECT_ID)
 
-# 100 per page is max, so we can iterate all pages like this:
-# issues.each_page do |page|
-#   page.each do |issue|
-#     p issue.id
-#   end
-# end
-# But paging like that causes it to not pay attention to
-# any filering above in the list_repo() method options.
-# So we have to break thing up into sub-100 chunks.
+total_issues = 0 # how many issues were processed?
 
-github = Github.new org: GITHUB_ORG, repo: GITHUB_REPO, login: GITHUB_LOGIN, password: GITHUB_PASSWORD
-issues = github.issues.list(:filter => 'all', :state => 'open', :sort => 'created')
+github = Octokit::Client.new(:login => GITHUB_LOGIN, :password => GITHUB_PASSWORD)
 
-i = 0
+issues_filter = 'bug'
 
-issues.each_page do |page|
-  page.each do |issue|
-    # labels = issue.labels.select { |k,v| k == "name" }
-    puts "issue: #{issue.title} (#{issue.number})"
+page_issues = 1
+issues = github.list_issues(GITHUB_REPO, { :page => page_issues, :labels => issues_filter } )
 
-    # puts JSON.pretty_generate(issue)
+while issues.count > 0
 
-    comments = github.issues.comments.list GITHUB_ORG, GITHUB_REPO, :issue_id => issue.number
-    comments.each_page do |comments_page|
-      comments_page.each do |comment|
-        puts "  comment: #{comment.body}"
-      end
+  issues.each do |issue|
+    total_issues += 1
+    comments = github.issue_comments(GITHUB_REPO, issue.number)
+    puts "issue: #{issue.number} #{issue.title}, with #{comments.count} comments"
+
+    comments.each do |comment|
+      puts "  #{comment.body} (#{comment.user.login})"
     end
-
-    # puts issue.body
 
     # # Create story in Pivotal Tracker
     # story = proj.stories.create(
@@ -85,26 +53,24 @@ issues.each_page do |page|
     #   # Omitting puts it in the Icebox.
     #   # 'unstarted' puts it in 'Current' if Commit Mode is on; 'Backlog' if Auto Mode is on.
     # )
-
-    # comments = github.issues.comments.all 'telvue', 'Connect', issue.number
-    # comments.each do |comment|
-    #   puts '--------------'
-    #   puts 'ADDING COMMENT'
-    #   puts 'From: ' + comment.user.login
-    #   puts 'Date: ' + comment.created_at
-    #   puts ''
-    #   puts comment.body
-
-      # story.notes.create(
-      #   text: comment.body.gsub(/\r\n\r\n/, "\n\n"),
-      #   author: comment.user.login,
-      #   noted_at: comment.created_at
-      # )
+    
+    # comments = github.issues.comments.list GITHUB_ORG, GITHUB_REPO, :issue_id => issue.number
+    # comments.each_page do |comments_page|
+    #   comments_page.each do |comment|
+    #     puts "  comment: #{comment.body}"
+    #     story.notes.create(
+    #       text: comment.body.gsub(/\r\n\r\n/, "\n\n"),
+    #       author: comment.user.login,
+    #       noted_at: comment.created_at
+    #     )
+    #   end
     # end
-    i = i + 1
+
   end
+
+  page_issues += 1
+  issues = github.list_issues(GITHUB_REPO, { :page => page_issues, :labels => issues_filter } )
+
 end
 
-puts '=============='
-puts 'TOTAL MIGRATED: ' + i.to_s
-puts '=============='
+puts "\n== processed #{total_issues} issues"
